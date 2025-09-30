bcba20aaec3010fc40488bf41f0feab737a42c31
gabrieldewabrata <gabrieldewabrata@tamu.edu>
2025-09-30 18:25:33 -0500

    added new page

--- manga-app/src/app/app.routes.ts
+++ manga-app/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => {
+            return import('./home/home').then((m) => m.Home)
+        }
+    },
+    {
+        path: 'profile',
+        pathMatch: 'full',
+        loadComponent: () => {
+            return import('./profile-page/profile-page').then((m) => m.ProfilePage)
+        }
+    }
+];
--- manga-app/src/app/app.ts
+++ manga-app/src/app/app.ts
@@ -1,15 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
-import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Home],
+  imports: [RouterOutlet, Header],
   template: `
     <app-header />
     <main>
-      <app-home />
+      <router-outlet />
     </main>
   `,
   styles: [
